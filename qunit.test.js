71eb3a53daa6ef05bc4ffc386b8eec7531b4c7d9
Elijah Williams <ekwilliams015@gmail.com>
2020-03-06 15:44:58 -0600

    add javascript to create sum and fix css for button

--- qunit.test.js
+++ qunit.test.js
@@ -1,10 +1,10 @@
 QUnit.module('MAIN MODULE', {})  // group all these tests together
 
 QUnit.test('TEST add', assert => {
-  assert.equal(add(1, 1), 2, 'Positive integers')
-  assert.equal(add(-1, -1), -2, 'Negative integers')
-  assert.equal(add(-10, 10), 0, 'Mixed')
-  assert.equal(add(23, -42), -19, 'Mixed2')
+  assert.equal(addNumbers(1, 1), 2, 'Positive integers')
+  assert.equal(addNumbers(-1, -1), -2, 'Negative integers')
+  assert.equal(addNumbers(-10, 10), 0, 'Mixed')
+  assert.equal(addNumbers(23, -42), -19, 'Mixed2')
 })
 
 
